96b43a83f7bbd063e666a128fcb81e0ef00e0b19
Göran Bäcklund <gbanm06@gmail.com>
2013-03-28 17:57:29 +0100

    Update ThreadsController.php

--- app/Controller/ThreadsController.php
+++ app/Controller/ThreadsController.php
@@ -1,9 +1,7 @@
 <?php
 App::uses('AppController', 'Controller');
 /**
- * Posts Controller
- *
- * @property Post $Post
+ 
  */
 
 class ThreadsController extends AppController {
@@ -24,11 +22,10 @@ class ThreadsController extends AppController {
                      
                      } else{
                      
-<<<<<<< HEAD
+
                          $this->Auth->allow('index', 'view', 'featured');
-=======
+
                          $this->Auth->allow('index', 'view', 'search', 'featured');
->>>>>>> 9a68e4b438b5fa86b48466ae879b206e2030f595
 
 
                      }
@@ -103,62 +100,11 @@ public function lock_thread($id = null, $boolean = null) {
 
 
 
-        public function feature_thread($id = null, $boolean = null) {
 
 
-	 if ($this->Auth->user('roles') == 'admin'){
-
-
-            if ($boolean==1){
-                    $this->Thread->updateAll(array('thread.Sticky'=> false ),
-    array('thread.id' => $id));
-
-
-                  $this->redirect(array('action' => 'index'));
-
-
-		             	}else{
-                                
-                                  $this->Thread->updateAll(array('thread.Sticky'=> true ),
-    array('thread.id' => $id));
-
-
-                  $this->redirect(array('action' => 'index'));
-                                }
                                      
                                      
-                                     
-                                     
-                                     
-                                      } }
-public function lock_thread($id = null, $boolean = null) {
 
-
-	 if ($this->Auth->user('roles') == 'admin'){
-
-
-            if ($boolean==1){
-                    $this->Thread->updateAll(array('thread.Locked'=> false ),
-    array('thread.id' => $id));
-
-
-                  $this->redirect(array('action' => 'index'));
-
-
-		             	}else{
-                                
-                                  $this->Thread->updateAll(array('thread.Locked'=> true ),
-    array('thread.id' => $id));
-
-
-                  $this->redirect(array('action' => 'index'));
-                                }
-                                     
-                                     
-                                     
-                                     
-                                     
-                                      } }
 
 
 	public function index() {
@@ -166,10 +112,6 @@ public function lock_thread($id = null, $boolean = null) {
 
 		$this->Thread->recursive = 1;
  	
-<<<<<<< HEAD
-
-=======
->>>>>>> 9a68e4b438b5fa86b48466ae879b206e2030f595
 
                      
 
